9c9f5cff863541c53036491c465f7fb7f250f455
Carlos Suarez del Real <csdelreal@gmail.com>
2021-12-29 17:23:45 -0600

    👌 IMPROVE: Convert to line chart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css'
-import Chart from './Chart'
+import './App.css';
+import Chart from './Chart';
 
-import { chicago_population, chicago_homicides } from './data'
+import { chicago_population, chicago_homicides } from './data';
 
 const populationChartConfig = {
 	type: 'line',
 	data: {
 		labels: chicago_population.map(item => item.year),
-		datasets: [{
-			label: 'Chicago Population',
-			data: chicago_population.map(item => item.population),
-			backgroundColor: 'rgba(255, 99, 132, 0.2)',
-			borderColor: 'rgba(255, 99, 132, 1)',
-			borderWidth: 1
-		}]
+		datasets: [
+			{
+				label: 'Chicago Population',
+				data: chicago_population.map(item => item.population),
+				borderColor: '#8e5ea2',
+				borderWidth: 2,
+				fill: false,
+			},
+		],
 	},
 	options: {
 		tooltips: {
@@ -25,42 +27,46 @@ const populationChartConfig = {
 					value = value.split(/(?=(?:...)*$)/);
 					value = value.join(',');
 					return value;
-				}
-			}
+				},
+			},
 		},
 		scales: {
-			yAxes: [{
-				ticks: {
-					beginAtZero: true,
-					userCallback: (value) => {
-						// Convert the number to a string and split the string every 3 charaters from the end
-						value = value.toString();
-						value = value.split(/(?=(?:...)*$)/);
-						value = value.join(',');
-						return value;
-					}
-				}
-			}]
-		}
-	}
-}
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+						userCallback: value => {
+							// Convert the number to a string and split the string every 3 charaters from the end
+							value = value.toString();
+							value = value.split(/(?=(?:...)*$)/);
+							value = value.join(',');
+							return value;
+						},
+					},
+				},
+			],
+		},
+	},
+};
 
 const homicidesChartConfig = {
 	type: 'line',
 	data: {
 		labels: chicago_homicides.map(item => item.year),
-		datasets: [{
-			label: 'Chicago Homicide Statistics',
-			data: chicago_homicides.map(item => item.numbers),
-			backgroundColor: 'rgba(255, 99, 132, 0.2)',
-			borderColor: 'rgba(255, 99, 132, 1)',
-			borderWidth: 1
-		}]
+		datasets: [
+			{
+				label: 'Chicago Homicide Statistics',
+				data: chicago_homicides.map(item => item.numbers),
+				borderColor: '#3e95cd',
+				borderWidth: 2,
+				fill: false,
+			},
+		],
 	},
 	options: {
 		// ...
-	}
-}
+	},
+};
 
 function App() {
 	return (
@@ -68,10 +74,10 @@ function App() {
 			<h1>Chicago Data</h1>
 			<h2>Population</h2>
 			<Chart config={populationChartConfig} />
-			<h2>Homicide Statistics</h2>
+			<h2>Homicides</h2>
 			<Chart config={homicidesChartConfig} />
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
--- src/data.js
+++ src/data.js
@@ -2,354 +2,363 @@ export const chicago_population = [
 	{
 		year: 1840,
 		population: 4470,
-		change: '—'
+		change: '—',
 	},
 	{
 		year: 1850,
-		population:	29963,
-		change: '570.3%'
+		population: 29963,
+		change: '570.3%',
 	},
 	{
 		year: 1860,
 		population: 112172,
-		change: '274.4%'
+		change: '274.4%',
 	},
 	{
 		year: 1870,
 		population: 298977,
-		change: '166.5%'
+		change: '166.5%',
 	},
 	{
 		year: 1880,
 		population: 503185,
-		change: '68.3 %'
+		change: '68.3 %',
 	},
 	{
 		year: 1890,
-		population:	1099850,
-		change: '118.6 %'
+		population: 1099850,
+		change: '118.6 %',
 	},
 	{
 		year: 1900,
-		population:	1698575,
-		change: '54.4 %'
+		population: 1698575,
+		change: '54.4 %',
 	},
 	{
 		year: 1910,
-		population:	2185283,
-		change:	'28.7 %'
+		population: 2185283,
+		change: '28.7 %',
 	},
 	{
 		year: 1920,
-		population:	2701705,
-		change:'23.6 %'
+		population: 2701705,
+		change: '23.6 %',
 	},
 	{
 		year: 1930,
 		population: 3376438,
-		change: '25.0 %'
+		change: '25.0 %',
 	},
 	{
 		year: 1940,
 		population: 3396808,
-		change:	'0.6 %'
+		change: '0.6 %',
 	},
 	{
 		year: 1950,
-		population:	3620962,
-		change:	'6.6 %'
+		population: 3620962,
+		change: '6.6 %',
 	},
 	{
 		year: 1960,
-		population:	3550404,
-		change: '−1.9 %'
+		population: 3550404,
+		change: '−1.9 %',
 	},
 	{
 		year: 1970,
 		population: 3366957,
-		change: '−5.2 %'
+		change: '−5.2 %',
 	},
 	{
 		year: 1980,
-		population:	3005072,
-		change: '−10.7 %'
+		population: 3005072,
+		change: '−10.7 %',
 	},
 	{
 		year: 1990,
-		population:	2783726,
-		change: '−7.4 %'
+		population: 2783726,
+		change: '−7.4 %',
 	},
 	{
 		year: 2000,
-		population:	2896016,
-		change: '4.0 %'
+		population: 2896016,
+		change: '4.0 %',
 	},
 	{
 		year: 2010,
 		population: 2695598,
-		change: '−6.9 %'
+		change: '−6.9 %',
 	},
-]
+	{
+		year: 2020,
+		population: 2746388,
+		change: '1.9 %',
+	},
+];
 
 export const chicago_homicides = [
 	{
 		year: 1928,
-		numbers: 498
+		numbers: 498,
 	},
 	{
 		year: 1957,
-		numbers: 296
+		numbers: 296,
 	},
 	{
 		year: 1958,
-		numbers: 305
+		numbers: 305,
 	},
 	{
 		year: 1959,
-		numbers: 331
+		numbers: 331,
 	},
 	{
 		year: 1960,
-		numbers: 372
+		numbers: 372,
 	},
 	{
 		year: 1961,
-		numbers: 362
+		numbers: 362,
 	},
 	{
 		year: 1962,
-		numbers: 385
+		numbers: 385,
 	},
 	{
 		year: 1963,
-		numbers: 361
+		numbers: 361,
 	},
 	{
 		year: 1964,
-		numbers: 390
+		numbers: 390,
 	},
 	{
 		year: 1965,
-		numbers: 400
+		numbers: 400,
 	},
 	{
 		year: 1966,
-		numbers: 510
+		numbers: 510,
 	},
 	{
 		year: 1967,
-		numbers: 552
+		numbers: 552,
 	},
 	{
 		year: 1968,
-		numbers: 647
+		numbers: 647,
 	},
 	{
 		year: 1969,
-		numbers: 715
+		numbers: 715,
 	},
 	{
 		year: 1970,
-		numbers: 810
+		numbers: 810,
 	},
 	{
 		year: 1971,
-		numbers: 824
+		numbers: 824,
 	},
 	{
 		year: 1972,
-		numbers: 711
+		numbers: 711,
 	},
 	{
 		year: 1973,
-		numbers: 864
+		numbers: 864,
 	},
 	{
 		year: 1974,
-		numbers: 970
+		numbers: 970,
 	},
 	{
 		year: 1975,
-		numbers: 818
+		numbers: 818,
 	},
 	{
 		year: 1976,
-		numbers: 814
+		numbers: 814,
 	},
 	{
 		year: 1977,
-		numbers: 823
+		numbers: 823,
 	},
 	{
 		year: 1978,
-		numbers: 787
+		numbers: 787,
 	},
 	{
 		year: 1979,
-		numbers: 856
+		numbers: 856,
 	},
 	{
 		year: 1980,
-		numbers: 863
+		numbers: 863,
 	},
 	{
 		year: 1981,
-		numbers: 877
+		numbers: 877,
 	},
 	{
 		year: 1982,
-		numbers: 668
+		numbers: 668,
 	},
 	{
 		year: 1983,
-		numbers: 729
+		numbers: 729,
 	},
 	{
 		year: 1984,
-		numbers: 741
+		numbers: 741,
 	},
 	{
 		year: 1985,
-		numbers: 666
+		numbers: 666,
 	},
 	{
 		year: 1986,
-		numbers: 744
+		numbers: 744,
 	},
 	{
 		year: 1987,
-		numbers: 691
+		numbers: 691,
 	},
 	{
 		year: 1988,
-		numbers: 660
+		numbers: 660,
 	},
 	{
 		year: 1989,
-		numbers: 742
+		numbers: 742,
 	},
 	{
 		year: 1990,
-		numbers: 851
+		numbers: 851,
 	},
 	{
 		year: 1991,
-		numbers: 928
+		numbers: 928,
 	},
 	{
 		year: 1992,
-		numbers: 943
+		numbers: 943,
 	},
 	{
 		year: 1993,
-		numbers: 855
+		numbers: 855,
 	},
 	{
 		year: 1994,
-		numbers: 931
+		numbers: 931,
 	},
 	{
 		year: 1995,
-		numbers: 828
+		numbers: 828,
 	},
 	{
 		year: 1996,
-		numbers: 796
+		numbers: 796,
 	},
 	{
 		year: 1997,
-		numbers: 761
+		numbers: 761,
 	},
 	{
 		year: 1998,
-		numbers: 704
+		numbers: 704,
 	},
 	{
 		year: 1999,
-		numbers: 643
+		numbers: 643,
 	},
 	{
 		year: 2000,
-		numbers: 633
+		numbers: 633,
 	},
 	{
 		year: 2001,
-		numbers: 667
+		numbers: 667,
 	},
 	{
 		year: 2002,
-		numbers: 656
+		numbers: 656,
 	},
 	{
 		year: 2003,
-		numbers: 601
+		numbers: 601,
 	},
 	{
 		year: 2004,
-		numbers: 453
+		numbers: 453,
 	},
 	{
 		year: 2005,
-		numbers: 451
+		numbers: 451,
 	},
 	{
 		year: 2006,
-		numbers: 471
+		numbers: 471,
 	},
 	{
 		year: 2007,
-		numbers: 448
+		numbers: 448,
 	},
 	{
 		year: 2008,
-		numbers: 513
+		numbers: 513,
 	},
 	{
 		year: 2009,
-		numbers: 459
+		numbers: 459,
 	},
 	{
 		year: 2010,
-		numbers: 436
+		numbers: 436,
 	},
 	{
 		year: 2011,
-		numbers: 433
+		numbers: 433,
 	},
 	{
 		year: 2012,
-		numbers: 532
+		numbers: 532,
 	},
 	{
 		year: 2013,
-		numbers: 415
+		numbers: 415,
 	},
 	{
 		year: 2014,
-		numbers: 415
+		numbers: 415,
 	},
 	{
 		year: 2015,
-		numbers: 468
+		numbers: 468,
 	},
 	{
 		year: 2016,
-		numbers: 771
+		numbers: 771,
 	},
 	{
 		year: 2017,
-		numbers: 653
+		numbers: 653,
 	},
 	{
 		year: 2018,
-		numbers: 561
+		numbers: 561,
 	},
 	{
 		year: 2019,
-		numbers: 518
+		numbers: 518,
 	},
 	{
 		year: 2020,
-		numbers: 769
+		numbers: 769,
 	},
-]
+	{
+		year: 2021,
+		numbers: 793,
+	},
+];
